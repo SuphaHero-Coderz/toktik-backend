b066c80b9f53ac7663997e3bab17b2395eb9c1d1
mythicalbadger <jussy12.work@gmail.com>
2023-10-17 16:50:21 +0700

    Generate presigned url from AWS

--- src/main.py
+++ src/main.py
@@ -1,84 +1,46 @@
-from typing import Union
-
-from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from pydantic import BaseModel
-
-fake_users_db = {
-    "suphawizard": {
-        "username": "suphawizard",
-        "full_name": "Suphawit Wongfu",
-        "email": "dannyboy@sharklaser.com",
-        "hashed_password": "superhardpass",
-        "disabled": False,
-    },
-}
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import os
+import boto3
+from botocore.exceptions import NoCredentialsError
 
 app = FastAPI()
 
+load_dotenv()
 
-def fake_hash_password(password: str):
-    return "super" + password
+s3 = boto3.client('s3', 
+    aws_access_key_id = os.getenv("AWS_ACCESS_KEY"),
+    aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY"),
+)
 
+origins = [
+    "http://localhost:3000"
+]
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-
-class User(BaseModel):
-    username: str
-    email: Union[str, None] = None
-    full_name: Union[str, None] = None
-    disabled: Union[bool, None] = None
-
-
-class UserInDB(User):
-    hashed_password: str
-
-
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
-
-
-def fake_decode_token(token):
-    # This doesn't provide any security at all
-    # Check the next version
-    user = get_user(fake_users_db, token)
-    return user
-
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    user = fake_decode_token(token)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authentication credentials",
-            headers={"WWW-Authenticate": "Bearer"},
+@app.get("/generate_presigned_url/{object_key}")
+async def generate_presigned_url(object_key: str):
+    try:
+        # Generate a presigned URL for the S3 object
+        presigned_url = s3.generate_presigned_url(
+            'get_object',
+            Params={
+                'Bucket': os.getenv("BUCKET_NAME"),
+                'Key': object_key,
+            },
+            ExpiresIn=3600  # The URL will expire in 1 hour
         )
-    return user
 
+        return {"presigned_url": presigned_url}
 
-async def get_current_active_user(current_user: User = Depends(get_current_user)):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
-
-
-@app.post("/token")
-async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    user_dict = fake_users_db.get(form_data.username)
-    if not user_dict:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    user = UserInDB(**user_dict)
-    hashed_password = fake_hash_password(form_data.password)
-    if not hashed_password == user.hashed_password:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-
-    return {"access_token": user.username, "token_type": "bearer"}
-
-
-@app.get("/users/me")
-async def read_users_me(current_user: User = Depends(get_current_active_user)):
-    return current_user
+    except NoCredentialsError:
+        return {"error": "AWS credentials not available."}
 
