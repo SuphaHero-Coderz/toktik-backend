f33b3997715e4c9267063d6d7a3b39bc5302a5f4
Suphawit <suphawit.won@gmail.com>
2023-10-17 23:38:06 +0700

    Containerized Backend and Added Redis Message Broker logic

--- src/main.py
+++ src/main.py
@@ -1,84 +1,51 @@
-from typing import Union
+import json
+import os
+import os
 
-from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+import redis
+from fastapi import FastAPI
 from pydantic import BaseModel
-
-fake_users_db = {
-    "suphawizard": {
-        "username": "suphawizard",
-        "full_name": "Suphawit Wongfu",
-        "email": "dannyboy@sharklaser.com",
-        "hashed_password": "superhardpass",
-        "disabled": False,
-    },
-}
 
 app = FastAPI()
 
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
-def fake_hash_password(password: str):
-    return "super" + password
+class RedisResource:
+    REDIS_QUEUE_LOCATION = os.getenv('REDIS_QUEUE', 'localhost')
+    CHUNK_QUEUE = 'queue:chunk'
+    ENCODE_QUEUE = 'queue:encode'
+    THUMBNAIL_QUEUE = 'queue:thumbnail'
 
+    host, *port_info = REDIS_QUEUE_LOCATION.split(':')
+    port = tuple()
+    if port_info:
+        port, *_ = port_info
+        port = (int(port),)
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+    conn = redis.Redis(host=host, *port)
 
+class Item(BaseModel):
+    name: str
 
-class User(BaseModel):
-    username: str
-    email: Union[str, None] = None
-    full_name: Union[str, None] = None
-    disabled: Union[bool, None] = None
+@app.post("/chunk")
+def chunk(item: Item):
+    RedisResource.conn.rpush(
+        RedisResource.CHUNK_QUEUE,
+        json.dumps(item.__dict__))
+    return item
 
+@app.post("/encode")
+def chunk(item: Item):
+    RedisResource.conn.rpush(
+        RedisResource.ENCODE_QUEUE,
+        json.dumps(item.__dict__))
+    return item
 
-class UserInDB(User):
-    hashed_password: str
-
-
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
-
-
-def fake_decode_token(token):
-    # This doesn't provide any security at all
-    # Check the next version
-    user = get_user(fake_users_db, token)
-    return user
-
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    user = fake_decode_token(token)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authentication credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    return user
-
-
-async def get_current_active_user(current_user: User = Depends(get_current_user)):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
-
-
-@app.post("/token")
-async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    user_dict = fake_users_db.get(form_data.username)
-    if not user_dict:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    user = UserInDB(**user_dict)
-    hashed_password = fake_hash_password(form_data.password)
-    if not hashed_password == user.hashed_password:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-
-    return {"access_token": user.username, "token_type": "bearer"}
-
-
-@app.get("/users/me")
-async def read_users_me(current_user: User = Depends(get_current_active_user)):
-    return current_user
-
+@app.post("/thumbnail")
+def chunk(item: Item):
+    RedisResource.conn.rpush(
+        RedisResource.THUMBNAIL_QUEUE,
+        json.dumps(item.__dict__))
+    return item
