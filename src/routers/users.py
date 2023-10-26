c25bf7fdd1c1773c47d636914239416d4e775182
Suphawit <suphawit.won@gmail.com>
2023-10-26 22:03:28 +0700

    fixed users router recursion

--- src/routers/users.py
+++ src/routers/users.py
@@ -1,43 +1,43 @@
-from fastapi import APIRouter
-from ..schemas import  UserCreate, User
-from ..db_services import get_db_session, authenticate_user, create_token, get_user, delete_user, get_user_by_username, get_current_user
 import fastapi as _fastapi
-import sqlalchemy.orm as _orm
 import fastapi.security as _security
+import sqlalchemy.orm as _orm
+from fastapi import APIRouter
 
+import src.db_services as _services
+import src.schemas as _schemas
 
 router = APIRouter(tags=["users"])
 @router.post("/api/users")
-async  def create_user(user: UserCreate, db: _orm.Session = _fastapi.Depends(get_db_session)):
-    db_user = await get_user_by_username(user.username, db)
+async  def create_user(user: _schemas.UserCreate, db: _orm.Session = _fastapi.Depends(_services.get_db_session)):
+    db_user = await _services.get_user_by_username(user.username, db)
     if db_user:
         raise _fastapi.HTTPException(status_code=400, detail="Username already in use")
-    user = await create_user(user, db)
-    return await create_token(user)
+    user = await _services.create_user(user, db)
+    return await _services.create_token(user)
 
 @router.post("/api/token")
 async def generate_token(form_data: _security.OAuth2PasswordRequestForm = _fastapi.Depends(),
-                         db: _orm.Session = _fastapi.Depends(get_db_session)):
-    user = await authenticate_user(username=form_data.username, password=form_data.password, db=db)
+                         db: _orm.Session = _fastapi.Depends(_services.get_db_session)):
+    user = await _services.authenticate_user(username=form_data.username, password=form_data.password, db=db)
     if not user:
         raise _fastapi.HTTPException(status_code=401, detail="Invalid Credentials")
-    return await create_token(user)
+    return await _services.create_token(user)
 
-@router.get("/api/users/me", response_model= User)
-async def get_current_user(user: User = _fastapi.Depends(get_current_user)):
+@router.get("/api/users/me", response_model=_schemas.User)
+async def get_current_user(user: _schemas.User = _fastapi.Depends(_services.get_current_user)):
     return user
 
-@router.get("/api/users/{user_id}", response_model= User)
+@router.get("/api/users/{user_id}", response_model=_schemas.User)
 async def get_user(
         user_id: int,
-        current_user: User = _fastapi.Depends(get_current_user),
-        db: _orm.Session = _fastapi.Depends(get_db_session)):
-    return await get_user(user_id=user_id, current_user=current_user, db=db)
+        current_user: _schemas.User = _fastapi.Depends(_services.get_current_user),
+        db: _orm.Session = _fastapi.Depends(_services.get_db_session)):
+    return await _services.get_user(user_id=user_id, current_user=current_user, db=db)
 
 @router.delete("/api/users/{user_id}", status_code=204)
 async def delete_user(
         user_id: int,
-        current_user: User = _fastapi.Depends(get_current_user),
-        db: _orm.Session = _fastapi.Depends(get_db_session)):
-    await delete_user(user_id=user_id, current_user=current_user, db=db)
+        current_user: _schemas.User = _fastapi.Depends(_services.get_current_user),
+        db: _orm.Session = _fastapi.Depends(_services.get_db_session)):
+    await _services.delete_user(user_id=user_id, current_user=current_user, db=db)
     return {"message", "Successfully Deleted"}
